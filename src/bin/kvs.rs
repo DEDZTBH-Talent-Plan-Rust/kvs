f9967d4eb891df1294c35912c48cd22e66ccb073
DEDZTBH <peiqial@outlook.com>
2021-01-31 21:48:52 +0800

    passing cli_* tests

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,53 @@
+use clap::Clap;
+use std::process::exit;
+
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "DEDZTBH <peiqial@outlook.com>")]
+pub struct Opt {
+    #[clap(subcommand)]
+    subcmd: SubCommand
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    #[clap(version = "0.1.0", author = "DEDZTBH <peiqial@outlook.com>")]
+    Set(SetCmd),
+    #[clap(version = "0.1.0", author = "DEDZTBH <peiqial@outlook.com>")]
+    Get(GetCmd),
+    #[clap(version = "0.1.0", author = "DEDZTBH <peiqial@outlook.com>")]
+    Rm(RmCmd),
+}
+
+#[derive(Clap)]
+struct SetCmd {
+    key: String,
+    value: String,
+}
+
+#[derive(Clap)]
+struct GetCmd {
+    key: String
+}
+
+#[derive(Clap)]
+struct RmCmd {
+    key: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::parse();
+    match opt.subcmd {
+        SubCommand::Set(cmd) => {
+            eprintln!("unimplemented");
+            exit(-1)
+        }
+        SubCommand::Get(cmd) => {
+            eprintln!("unimplemented");
+            exit(-1)
+        }
+        SubCommand::Rm(cmd) => {
+            eprintln!("unimplemented");
+            exit(-1)
+        }
+    }
 }
